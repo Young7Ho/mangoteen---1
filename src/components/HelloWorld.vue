eb6d6491e022cfdaa3526986b9a63356fee8fa8c
Young7Ho <953417678@qq.com>
2023-02-14 15:42:43 +0800

    init2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1></h1>
 
   <p>
     Recommended IDE setup:
@@ -26,7 +26,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" >count is: {{ }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
