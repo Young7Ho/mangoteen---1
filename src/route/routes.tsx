cced5bba0fa8ea0b0922f69d88662135b77baec7
Young7Ho <953417678@qq.com>
2023-02-15 11:05:31 +0800

    修改默认路由

--- src/route/routes.tsx
+++ src/route/routes.tsx
@@ -9,12 +9,13 @@ import { Welcome } from '../views/Welcome';
 
 
 export const routes:RouteRecordRaw[] = [
-    { path: '/', component: Foo },
-    { path: '/bar', component: Bar },
+    { path: '/', redirect: '/welcome' },
+
     {
         path: '/welcome',
         component: Welcome,
         children: [
+            {path: '', redirect: '/welcome/1'},
             {path:'1',component: First},
             { path: '2', component: Second, },
             { path: '3', component: Third, },
