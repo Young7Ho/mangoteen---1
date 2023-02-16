54f8158b269576726e16aed8989e0c917a2fd18a
Young7Ho <953417678@qq.com>
2023-02-16 21:23:59 +0800

    完成 overlay 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,16 +1,20 @@
 import { defineComponent , defineProps, PropType} from 'vue';
 import s from './Icon.module.scss' 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'saveMoneyCan'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' 
+| 'mangosteen' | 'saveMoneyCan' | 'menu' | 'export' | 'notify'
 export const Icon = defineComponent({
  props: {
     name: {
         type: String as PropType<IconName>,
         require: true
     },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
+    }
  },
  setup: (props,context) => {
   return () => (
-    <svg class={s.icon}>
+    <svg class={s.icon} onClick={props.onClick}>
         <use xlinkHref={'#' + props.name}></use>
     </svg>
    )
