e6e611fb03dfa9f93912a6cb802e78b4aa1c833a
Young7Ho <953417678@qq.com>
2023-02-21 22:58:19 +0800

    完成了退出登录、显示当前用户的功能

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,7 +1,9 @@
-import { defineComponent , PropType, ref } from 'vue';
+import { defineComponent, PropType, ref, onMounted } from 'vue';
 import s from './Overlay.module.scss' 
 import { Icon } from './Icon';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+import { mePromise } from './me';
+import { Dialog } from 'vant';
 export const Overlay = defineComponent({
   props:{
     onClose: {
@@ -13,39 +15,61 @@ export const Overlay = defineComponent({
         props.onClose?.()
     }
     const onClickSignIn = () => {}
+    const route = useRoute()
+    const me = ref<User>()
+    onMounted(async () => {
+      const response = await mePromise
+      me.value = response?.data.resource
+    })
+    const onSignOut = async () => {
+      await Dialog.confirm({
+        title: '确认',
+        message: '您确定退出登录吗？'
+      })
+      localStorage.removeItem('jwt')
+    }
   return () => (
     <>
-        <div class={s.mask} onClick={close}></div>
-      <div class={s.overlay}>
-        <section class={s.currentUser} onClick={onClickSignIn}>
-          <h2>未登录用户</h2>
-          <p>点击这里登录</p>
-        </section>
-        <nav>
-          <ul class={s.action_list}>
-            <li >
-                <RouterLink to="/statistics" class={s.action}>
-                    <Icon name="chart" class={s.icon}/>
-                    <span>统计图表</span>
-                </RouterLink>
-            </li>
-            <li>
-                <RouterLink to="/export" class={s.action}>
-                    <Icon name="export"  class={s.icon} />
-                    <span>导出数据</span>
-                </RouterLink>
-            </li>
-            <li>
-                <RouterLink to="/notify" class={s.action}>
-                    <Icon name="notify" class={s.icon}/>
-                    <span>记账提醒</span>
-                </RouterLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div class={s.mask} onClick={close}></div>
+    <div class={s.overlay}>
+      <section class={s.currentUser}>
+        {me.value ? (
+          <div>
+            <h2 class={s.email}>{me.value.email}</h2>
+            <p onClick={onSignOut}>点击这里退出登录</p>
+          </div>
+        ) : (
+          <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+            <h2>未登录用户</h2>
+            <p>点击这里登录</p>
+          </RouterLink>
+           )}
+           </section>
+           <nav>
+             <ul class={s.action_list}>
+               <li>
+                 <RouterLink to="/statistics" class={s.action}>
+                   <Icon name="chart" class={s.icon} />
+                   <span>统计图表</span>
+                 </RouterLink>
+               </li>
+               <li>
+                 <RouterLink to="/export" class={s.action}>
+                   <Icon name="export" class={s.icon} />
+                   <span>导出数据</span>
+                 </RouterLink>
+               </li>
+               <li>
+                 <RouterLink to="/notify" class={s.action}>
+                   <Icon name="notify" class={s.icon} />
+                   <span>记账提醒</span>
+                 </RouterLink>
+               </li>
+             </ul>
+           </nav>
+         </div>
     </>
-   )
+  )
   }
 })
 
