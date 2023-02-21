2efc64d7a582a431a62f7434edbc61d6d5ab002e
Young7Ho <953417678@qq.com>
2023-02-21 21:09:53 +0800

    封装DateTIme组件

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -4,6 +4,7 @@ import { FloatButton } from '../../shared/FloatButton';
 import { http } from '../../shared/Http';
 import { Money } from '../../shared/Money';
 import s from './ItemSummary.module.scss';
+import { DateTime } from '../../shared/DateTime';
 export const ItemSummary = defineComponent({
   props: {
     startDate: {
@@ -62,7 +63,7 @@ export const ItemSummary = defineComponent({
                       <span class={s.tag}>{item.tags_id[0]}</span>
                       <span class={s.amount}>￥<Money value={item.amount} /></span>
                     </div>
-                    <div class={s.time}>{item.happen_at}</div>
+                    <div class={s.time}><DateTime value={item.happen_at} format={"YYYY-MM-DD"}/></div>
                   </div>
                 </li>
               ))}
