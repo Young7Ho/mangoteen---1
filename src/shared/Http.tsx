0cbd6d438064aa30f0e44394430d6c30b4e20790
Young7Ho <953417678@qq.com>
2023-02-21 19:54:29 +0800

    mock tagEdit的数据

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { mockItemCreate, mockSession , mockTagIndex, mockTagShow} from "../mock/mock";
+import { mockItemCreate, mockSession , mockTagEdit, mockTagIndex, mockTagShow} from "../mock/mock";
 
 type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
 type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
@@ -42,9 +42,9 @@ const mock = (response: AxiosResponse) => {
     case 'itemCreate':
       [response.status, response.data] = mockItemCreate(response.config)
       return true
-    // case 'itemIndex':
-    //   [response.status, response.data] = mockItemIndex(response.config)
-    //   return true
+    case 'tagEdit':
+      [response.status, response.data] = mockTagEdit(response.config)
+      return true
     // case 'tagCreate':
     //   [response.status, response.data] = mockTagCreate(response.config)
     case 'session':
